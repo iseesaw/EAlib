283e78d64981367e3b0a423cfde85966bea566e8
iseesaw <18846080950@163.com>
2019-09-22 14:53:26 +0800

    remove all

--- EAlib/operators/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2019-09-21 20:55:17
-# @Author  : Kaiyan Zhang (kyzhang@ir.hit.edu.cn)
-# @Link    : https://github.com/iseesaw
-# @Version : v1.0
